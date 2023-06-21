1ea3273f23567b60f383de9cc300bcdf849ad862
Walterhart <walterwhart1@gmail.com>
2023-06-21 14:32:13 -0400

    Add css

---
+++ tenzies/src/components/Footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>This is the footer component.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ tenzies/src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+      <nav className="navbar spacing">
+        <div className="logo">Logo</div>
+        <ul className="nav-links">
+          <li><a className="nav-link" href="#">Home</a></li>
+          <li><a className="nav-link" href="#">About Site</a></li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
