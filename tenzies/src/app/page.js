8a0d90ff7d15503582009ba7e8f5bd01c0bf3f27
Walterhart <walterwhart1@gmail.com>
2023-06-23 02:56:48 -0400

    Add random starting dices

--- tenzies/src/app/page.js
+++ tenzies/src/app/page.js
@@ -1,32 +1,40 @@
+"use client"
 import Dice from '@/components/Dice';
 import styles from '../styles/page.module.css';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [dices, setDices] = useState([]);
+  const min = 0;
+  const max = 10;
+
+  const getRandomDiceArray = () => {
+    const randomDiceArray = Array(10).fill(1).map(() => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    });
+
+    return randomDiceArray;
+  };
+
+  useEffect(() => {
+    setDices(getRandomDiceArray());
+  }, []);
+
+  const dicesElement = dices.map((num, index) => (
+    <div key={index} className={styles['dice--box']}>
+      <Dice value={num} />
+    </div>
+  ));
+
   return (
     <main className={styles.main}>
       <div className={styles['main--content']}>
         <div className={styles['dices--container']}>
           <div className={styles['dice--row']}>
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
+            {dicesElement.slice(0, 5)}
           </div>
           <div className={styles['dice--row']}>
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
-            <span className={styles['dice--number-space']} />
-            <Dice value={1} />
+            {dicesElement.slice(5, 10)} 
           </div>
         </div>
       </div>
--- tenzies/src/components/Dice.js
+++ tenzies/src/components/Dice.js
@@ -1,6 +1,7 @@
 import styles from '../styles/page.module.css';
 
 function Dice(props) {
+    
   return (
     <div  className={styles['dice--box']}>
     <h2 className={styles[`dice--font `]}>{props.value}</h2>
