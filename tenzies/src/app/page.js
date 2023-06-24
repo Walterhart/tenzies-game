c34642cba2e6718009ea81e17574813ac209f88f
Walterhart <walterwhart1@gmail.com>
2023-06-23 21:19:37 -0400

    Add hold dice functionality

--- tenzies/src/app/page.js
+++ tenzies/src/app/page.js
@@ -1,7 +1,8 @@
-"use client"
-import Dice from '@/components/Dice';
-import styles from '../styles/page.module.css';
-import { useState, useEffect } from 'react';
+"use client";
+import Dice from "@/components/Dice";
+import styles from "../styles/page.module.css";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 export default function Home() {
   const [dices, setDices] = useState([]);
@@ -9,55 +10,64 @@ export default function Home() {
   const max = 10;
 
   const getRandomDiceArray = () => {
-    const randomDiceArray = Array(10).fill(1).map((num,index) => {
-      return {value : Math.floor(Math.random() * (max - min + 1)) + min , isHold: false , id: index}
-    });
+    const randomDiceArray = Array(10)
+      .fill(1)
+      .map((num) => {
+        return generateRandomDices();
+      });
 
     return randomDiceArray;
+  };
+
+  const generateRandomDices = () => {
+    return {
+      value: Math.floor(Math.random() * (max - min + 1)) + min,
+      isHold: false,
+      id: nanoid(),
+    };
   };
 
   useEffect(() => {
     setDices(getRandomDiceArray());
   }, []);
 
-  const reRollDices = () =>{
-    setDices(getRandomDiceArray)
-  }
+  const reRollDices = () => {
+    setDices((prevDices) =>
+      prevDices.map((dice) => {
+        return dice.isHold ? dice : generateRandomDices();
+      })
+    );
+  };
 
   const dicesElement = dices.map((num) => (
-    <div key={num.id} className={styles['dice--box']}>
-      <Dice  value={num.value} isHold ={num.isHold} holdDice = {()=>holdDice(num.id)}/>
+    <div key={num.id} className={styles["dice--box"]}>
+      <Dice
+        value={num.value}
+        isHold={num.isHold}
+        holdDice={() => holdDice(num.id)}
+      />
     </div>
   ));
 
-
-  
   const holdDice = (id) => {
-    setDices(prevDice => {
-      const updatedDices = prevDice.map(num => {
-        if (num.id === id) {
-          return {
-            ...num,
-            isHold: !num.isHold
-          };
-        }
-        return num;
-      });
-      return updatedDices;
-    });
+    setDices((prevDice) =>
+      prevDice.map((dice) =>
+        dice.id === id ? { ...dice, isHold: !dice.isHold } : dice
+      )
+    );
   };
+
   return (
     <main className={styles.main}>
-      <div className={styles['main--content']}>
-        <div className={styles['dices--container']}>
-          <div className={styles['dice--row']}>
-            {dicesElement.slice(0, 5)}
-          </div>
-          <div className={styles['dice--row']}>
-            {dicesElement.slice(5, 10)} 
-          </div>
+      <div className={styles["main--content"]}>
+        <div className={styles["dices--container"]}>
+          <div className={styles["dice--row"]}>{dicesElement.slice(0, 5)}</div>
+          <div className={styles["dice--row"]}>{dicesElement.slice(5, 10)}</div>
         </div>
-        <button className={styles['reRoll--btn']} onClick={reRollDices}> Roll</button>
+        <button className={styles["reRoll--btn"]} onClick={reRollDices}>
+          {" "}
+          Roll
+        </button>
       </div>
     </main>
   );
