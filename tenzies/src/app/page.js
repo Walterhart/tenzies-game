683236d1638803fc808f2aae2183b0ba48433684
Walterhart <walterwhart1@gmail.com>
2023-06-23 21:31:48 -0400

    Re-organize functions logic

--- tenzies/src/app/page.js
+++ tenzies/src/app/page.js
@@ -1,20 +1,22 @@
 "use client";
+import React, { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 import Dice from "@/components/Dice";
 import styles from "../styles/page.module.css";
-import { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
 
 export default function Home() {
   const [dices, setDices] = useState([]);
   const min = 0;
   const max = 10;
 
+  useEffect(() => {
+    setDices(getRandomDiceArray());
+  }, []);
+
   const getRandomDiceArray = () => {
     const randomDiceArray = Array(10)
-      .fill(1)
-      .map((num) => {
-        return generateRandomDices();
-      });
+      .fill()
+      .map(() => generateRandomDices());
 
     return randomDiceArray;
   };
@@ -27,15 +29,17 @@ export default function Home() {
     };
   };
 
-  useEffect(() => {
-    setDices(getRandomDiceArray());
-  }, []);
-
   const reRollDices = () => {
     setDices((prevDices) =>
-      prevDices.map((dice) => {
-        return dice.isHold ? dice : generateRandomDices();
-      })
+      prevDices.map((dice) => (dice.isHold ? dice : generateRandomDices()))
+    );
+  };
+
+  const holdDice = (id) => {
+    setDices((prevDice) =>
+      prevDice.map((dice) =>
+        dice.id === id ? { ...dice, isHold: !dice.isHold } : dice
+      )
     );
   };
 
@@ -49,14 +53,6 @@ export default function Home() {
     </div>
   ));
 
-  const holdDice = (id) => {
-    setDices((prevDice) =>
-      prevDice.map((dice) =>
-        dice.id === id ? { ...dice, isHold: !dice.isHold } : dice
-      )
-    );
-  };
-
   return (
     <main className={styles.main}>
       <div className={styles["main--content"]}>
@@ -65,7 +61,6 @@ export default function Home() {
           <div className={styles["dice--row"]}>{dicesElement.slice(5, 10)}</div>
         </div>
         <button className={styles["reRoll--btn"]} onClick={reRollDices}>
-          {" "}
           Roll
         </button>
       </div>
